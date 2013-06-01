7b80d3b1a272b6e6ad3336e4ababafc7682d475f
Callum Jones <callum@connected.fm>
2013-06-01 20:46:30 +0800

    Set basic box background

--- app/models/suburb.js
+++ app/models/suburb.js
@@ -46,6 +46,7 @@ define([
         return this.attributes["info_window"];
 
       var boxText = document.createElement("div");
+      boxText.style.cssText = "background: white";
       var content_string = "<div class=\"info_box\">";
       content_string += "<div class=\"title\"><h2>" + this.attributes.suburb_name + "</h2></div>";
       content_string += "<div class=\"percent\">" + this.attributes.percent + "%</div>";
