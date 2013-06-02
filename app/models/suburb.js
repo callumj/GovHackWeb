a892b742e17365ea9a0a23d58ef148f0257cedd1
Callum Jones <callum@connected.fm>
2013-06-02 10:48:47 +0800

    Bind to box being clickable

--- app/models/suburb.js
+++ app/models/suburb.js
@@ -130,6 +130,9 @@ define([
       google.maps.event.addListener(context.buildInfoWindow(), 'domready', function() {
         $(".info_box").parent().css("position", "absolute");
         $(".info_box").parent().css("top", "0px");
+        $(".info_box").on("click", function() {
+          context.markerClickEvent(context, event);
+        })
       });
     },
 
